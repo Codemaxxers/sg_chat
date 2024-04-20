d8684c92b08b85b46e530417dbe02783205f8687
TheoH32 <theo.h131@gmail.com>
2024-04-20 11:51:39 -0700

    enemy health x10

--- src/main/java/com/nighthawk/spring_portfolio/mvc/enemy/Enemy.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/enemy/Enemy.java
@@ -1,9 +1,5 @@
 package com.nighthawk.spring_portfolio.mvc.enemy;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,6 +8,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
@@ -43,15 +42,15 @@ public class Enemy {
     public static List<Enemy> createInitialData() {
         ArrayList<Enemy> enemies = new ArrayList<>();
 
-        enemies.add(new Enemy("Pixie", 10, 1, 1, 1, "Grass"));
-        enemies.add(new Enemy("Troll", 20, 5, 2, 2, "Grass"));
-        enemies.add(new Enemy("Golem", 30, 2, 5, 5, "Grass"));
-        enemies.add(new Enemy("Goblin", 15, 10, 2, 5, "Dark"));
-        enemies.add(new Enemy("Ghost", 25, 8, 5, 7, "Dark"));
-        enemies.add(new Enemy("Dragon", 35, 12, 8, 10, "Dark"));
-        enemies.add(new Enemy("Snow Man", 35, 15, 10, 10, "Snow"));
-        enemies.add(new Enemy("Snow Golem", 35, 20, 10, 12, "Snow"));
-        enemies.add(new Enemy("Ice Wizard", 45, 25, 10, 15, "Snow"));
+        enemies.add(new Enemy("Pixie", 100, 1, 1, 1, "Grass"));
+        enemies.add(new Enemy("Troll", 200, 5, 2, 2, "Grass"));
+        enemies.add(new Enemy("Golem", 300, 2, 5, 5, "Grass"));
+        enemies.add(new Enemy("Goblin", 150, 10, 2, 5, "Dark"));
+        enemies.add(new Enemy("Ghost", 250, 8, 5, 7, "Dark"));
+        enemies.add(new Enemy("Dragon", 350, 12, 8, 10, "Dark"));
+        enemies.add(new Enemy("Snow Man", 350, 15, 10, 10, "Snow"));
+        enemies.add(new Enemy("Snow Golem", 350, 20, 10, 12, "Snow"));
+        enemies.add(new Enemy("Ice Wizard", 450, 25, 10, 15, "Snow"));
         return enemies;
     }
 
